43e0ff61835df1b3cc6ceb51297333d594b5f438
Mkambani <mukeshcode9587826831@gmail.com>
2026-03-25 16:45:19 +0530

    Update ContactPage.tsx

--- src/components/ContactPage.tsx
+++ src/components/ContactPage.tsx
@@ -18,10 +18,8 @@ export const ContactPage: React.FC = () => {
         <div className="relative bg-stone-100 dark:bg-slate-800 box-border flex flex-col max-w-full break-words w-full mx-auto px-2.5">
           <div className="box-border gap-x-2.5 flex flex-col grow flex-wrap h-full max-w-[min(100%,767px)] min-h-[auto] min-w-[auto] break-words gap-y-2.5 w-full mx-auto py-[70px] md:flex-nowrap md:max-w-[min(100%,1300px)] md:py-[110px]">
             <div className="relative box-border gap-x-[18px] flex flex-wrap justify-center min-h-[auto] break-words gap-y-[18px] w-full p-2.5 md:gap-x-[30px] md:flex-nowrap md:justify-between md:gap-y-[30px]">
-              <ContactInfoCard
-                title="Support Email"
-                icon="/assets/icon-41.svg"
-              >
+              
+              <ContactInfoCard title="Support Email" icon="/assets/icon-41.svg">
                 <a
                   href="mailto:infoexample@lohiacollege.edu"
                   className="hover:text-cyan-800 dark:hover:text-cyan-400 dark:text-cyan-400 transition-colors"
@@ -37,36 +35,27 @@ export const ContactPage: React.FC = () => {
                 </a>
               </ContactInfoCard>
 
-              <ContactInfoCard
-                title="Phone Number"
-                icon="/assets/icon-42-2.svg"
-              >
+              <ContactInfoCard title="Phone Number" icon="/assets/icon-42-2.svg">
                 <a
-                  href="tel:+91 9587826831"
+                  href="tel:+919521045443"
                   className="hover:text-cyan-800 dark:hover:text-cyan-400 dark:text-cyan-400 transition-colors"
                 >
                   (+91) 9521045443
                 </a>
                 <br />
                 <a
-                  href="tel: 91 9587826831"
+                  href="tel:+919521045443"
                   className="hover:text-cyan-800 dark:hover:text-cyan-400 dark:text-cyan-400 transition-colors"
                 >
                   + 91 9521045443
                 </a>
               </ContactInfoCard>
 
-              <ContactInfoCard
-                title="Address"
-                icon="/assets/icon-43-2.svg"
-              >
+              <ContactInfoCard title="Address" icon="/assets/icon-43-2.svg">
                 <p>Near suncity hotel churu 331001</p>
               </ContactInfoCard>
 
-              <ContactInfoCard
-                title="Admission"
-                icon="/assets/icon-44-1.svg"
-              >
+              <ContactInfoCard title="Admission" icon="/assets/icon-44-1.svg">
                 <a
                   href="mailto:admission@lohiacollege.edu"
                   className="hover:text-cyan-800 dark:hover:text-cyan-400 dark:text-cyan-400 transition-colors"
@@ -75,7 +64,7 @@ export const ContactPage: React.FC = () => {
                 </a>
                 <br />
                 <a
-                  href="tel:91 9587826831"
+                  href="tel:+919521045443"
                   className="hover:text-cyan-800 dark:hover:text-cyan-400 dark:text-cyan-400 transition-colors"
                 >
                   91 9521045443
@@ -87,10 +76,19 @@ export const ContactPage: React.FC = () => {
             <div className="relative box-border gap-x-[30px] flex flex-col flex-wrap min-h-[auto] break-words gap-y-5 w-full p-2.5 md:flex-row md:flex-nowrap">
               <ContactForm />
 
-              {/* Map */}
+              {/* Map Section */}
               <div className="relative box-border gap-x-5 flex flex-col flex-wrap min-h-[auto] break-words gap-y-5 w-full overflow-hidden md:flex-nowrap md:w-6/12">
-                <div className="relative box-border gap-x-5 max-w-full min-h-[auto] break-words gap-y-5 w-full h-full overflow-hidden rounded-xl">
-                  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3513.24271954381!2d74.96487707612849!3d28.29097069950809!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39149e027346269f%3A0xcd3564d2b0661b08!2sGovernment%20Lohia%20College!5e0!3m2!1sen!2ssg!4v1774436493688!5m2!1sen!2ssg" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                <div className="relative box-border gap-x-5 max-w-full min-h-[auto] break-words gap-y-5 w-full h-full overflow-hidden rounded-xl bg-gray-200">
+                  <iframe 
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3513.3888780718365!2d74.9654163!3d28.286395!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39147573d61b17b9%3A0xc3124976735c0cf5!2sLohia%20College%20Road%2C%20Churu!5e0!3m2!1sen!2sin!4v1700000000000" 
+                    width="100%" 
+                    height="100%" 
+                    className="min-h-[350px] md:min-h-[450px]"
+                    style={{ border: 0 }} 
+                    allowFullScreen 
+                    loading="lazy" 
+                    referrerPolicy="no-referrer-when-downgrade"
+                  ></iframe>
                 </div>
               </div>
             </div>
